eb9fba0c57a33516f3c76cf1c32af2fd1d1da53c
Tolga Bektaş <tolgabektas00@gmail.com>
2022-01-05 21:13:14 +0300

    index page's graphics added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,166 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expenditure;
+use App\Models\Income;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('index');
+        /* dd(date('m')); */
+        $total_month_income = 0;
+        $total_month_expenditure = 0;
+        $year_incomes = [];
+        $year_expenditures = [];
+        $incomes = Income::whereMonth('created_at', date('m'))->whereYear('created_at', date('Y'))->where('status', 1)->get('price');
+        foreach ($incomes as $income) {
+            $total_month_income += $income->price;
+        }
+        $expenditures = Expenditure::whereMonth('created_at', date('m'))->whereYear('created_at', date('Y'))->where('status', 1)->get('price');
+        foreach ($expenditures as $expenditure) {
+            $total_month_expenditure += $expenditure->price;
+        }
+
+        $total_year_income = 0;
+        $total_year_expenditure = 0;
+
+        $january = 0;
+        $february = 0;
+        $march = 0;
+        $april = 0;
+        $may = 0;
+        $june = 0;
+        $july = 0;
+        $august = 0;
+        $september = 0;
+        $october = 0;
+        $november = 0;
+        $december = 0;
+        $incomes = Income::whereYear('created_at', date('Y'))->where('status', 1)->orderBy('created_at')->get();
+        foreach ($incomes as $income) {
+            $total_year_income += $income->price;
+            switch (\Carbon\Carbon::parse($income->created_at)->format('F')) {
+                case 'January':
+                    $january += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $january;
+                    break;
+                case 'February':
+                    $february += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $february;
+                    break;
+                case 'March':
+                    $march += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $march;
+                    break;
+                case 'April':
+                    $april += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $april;
+                    break;
+                case 'May':
+                    $may += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $may;
+                    break;
+                case 'June':
+                    $june += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $june;
+                    break;
+                case 'July':
+                    $july += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $july;
+                    break;
+                case 'August':
+                    $august += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $august;
+                    break;
+                case 'September':
+                    $september += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $september;
+                    break;
+                case 'October':
+                    $october += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $october;
+                    break;
+                case 'November':
+                    $november += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $november;
+                    break;
+                case 'December':
+                    $december += $income->price;
+                    $year_incomes[\Carbon\Carbon::parse($income->created_at)->format('F')] = $december;
+                    break;
+            }
+        }
+
+
+        $expenditures = Expenditure::whereYear('created_at', date('Y'))->where('status', 1)->orderBy('created_at')->get();
+        $january = 0;
+        $february = 0;
+        $march = 0;
+        $april = 0;
+        $may = 0;
+        $june = 0;
+        $july = 0;
+        $august = 0;
+        $september = 0;
+        $october = 0;
+        $november = 0;
+        $december = 0;
+        foreach ($expenditures as $expenditure) {
+            $total_year_expenditure += $expenditure->price;
+            switch (\Carbon\Carbon::parse($expenditure->created_at)->format('F')) {
+                case 'January':
+                    $january += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $january;
+                    break;
+                case 'February':
+                    $february += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $february;
+                    break;
+                case 'March':
+                    $march += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $march;
+                    break;
+                case 'April':
+                    $april += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $april;
+                    break;
+                case 'May':
+                    $may += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $may;
+                    break;
+                case 'June':
+                    $june += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $june;
+                    break;
+                case 'July':
+                    $july += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $july;
+                    break;
+                case 'August':
+                    $august += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $august;
+                    break;
+                case 'September':
+                    $september += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $september;
+                    break;
+                case 'October':
+                    $october += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $october;
+                    break;
+                case 'November':
+                    $november += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $november;
+                    break;
+                case 'December':
+                    $december += $expenditure->price;
+                    $year_expenditures[\Carbon\Carbon::parse($expenditure->created_at)->format('F')] = $december;
+                    break;
+            }
+        }
+
+        return view('index', compact('total_month_income', 'total_month_expenditure', 'total_year_income', 'total_year_expenditure', 'year_incomes', 'year_expenditures'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,25 +7,42 @@ Income Expenditure
 @endsection
 @section('content')
 <div class="row">
-  <div class="col-md-3">
+  <div class="col-md-6">
+    {{-- donut chart --}}
+    <div class="row">
+   <div class="col-md-6">
     <!-- small box -->
     <div class="small-box bg-success">
       <div class="inner">
-        <h3>$ 150</h3>
+        <h3>${{ $total_month_income }}</h3>
         <p>Monthly Income</p>
       </div>
       <div class="icon">
         <i class="ion ion-bag"></i>
       </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      <a href="{{ route('income.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
     </div>
-  </div>  
+  </div>
+  <!-- ./col --> 
+  <div class="col-md-6">
+    <!-- small box -->
+    <div class="small-box bg-danger">
+      <div class="inner">
+        <h3>${{ $total_month_expenditure }}</h3>
+        <p>Monthly Expenditure</p>
+      </div>
+      <div class="icon">
+        <i class="ion ion-pie-graph"></i>
+      </div>
+      <a href="{{ route('expenditure.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
   <!-- ./col -->
-  <div class="col-md-3">
+  <div class="col-md-6">
     <!-- small box -->
     <div class="small-box bg-success">
       <div class="inner">
-        <h3>$ 44</h3>
+        <h3>${{ $total_year_income }}</h3>
         <p>Yearly Income</p>
       </div>
       <div class="icon">
@@ -34,54 +51,13 @@ Income Expenditure
       <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
     </div>
   </div>
-  <!-- ./col -->   
-   <div class="col-md-3">
-    <!-- small box -->
-    <div class="small-box bg-success">
-      <div class="inner">
-        <h3>$ 44</h3>
-        <p>Monthly Profit</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-person-add"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-   <!-- ./col -->
-   <div class="col-md-3">
-    <!-- small box -->
-    <div class="small-box bg-success">
-      <div class="inner">
-        <h3>$ 44</h3>
-        <p>Yearly Profit</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-person-add"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col --> 
-  <div class="col-md-3">
-    <!-- small box -->
-    <div class="small-box bg-danger">
-      <div class="inner">
-        <h3>$ 65</h3>
-        <p>Monthly Expenditure</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-pie-graph"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
   <!-- ./col -->
-  <div class="col-md-3">
+
+  <div class="col-md-6">
     <!-- small box -->
     <div class="small-box bg-danger">
       <div class="inner">
-        <h3>$ 65</h3>
+        <h3>${{ $total_year_expenditure }}</h3>
         <p>Yearly Expenditure</p>
       </div>
       <div class="icon">
@@ -89,39 +65,14 @@ Income Expenditure
       </div>
       <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
     </div>
-  </div>
-  <!-- ./col -->
-  <div class="col-md-3">
-    <!-- small box -->
-    <div class="small-box bg-danger">
-      <div class="inner">
-        <h3>$ 65</h3>
-
-        <p>Monthly Loss</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-pie-graph"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->    
-    <div class="col-md-3">
-      <!-- small box -->
-      <div class="small-box bg-danger">
-        <div class="inner">
-          <h3>$ 53</h3>
-  
-          <p>Yearly Loss</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-      </div>
-    </div>
+  </div> 
    <!-- ./col -->
+  </div>
    {{-- donut chart --}}
+  </div>
+   
+
+   {{-- pie chart --}}
    <div class="col-md-6">     
     <div class="card">
       <div class="card-header">
@@ -137,15 +88,15 @@ Income Expenditure
         </div>
       </div>
       <div class="card-body">
-        <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+        <canvas id="pieChart" style="min-height: 215px; height: 215px; max-height: 215px; max-width: 100%;"></canvas>
       </div>
       <!-- /.card-body -->
     </div>
    </div>
-   {{-- donut chart --}}
+   {{-- pie chart --}}
 
    {{-- bar chart --}}
-   <div class="col-md-6">
+   <div class="col-md-12">
     <div class="card">
       <div class="card-header">
         <h3 class="card-title">Current Year Income-Expenditure Graphics</h3>
@@ -189,7 +140,11 @@ Income Expenditure
           pointStrokeColor    : 'rgba(220,53,69,1)',
           pointHighlightFill  : '#fff',
           pointHighlightStroke: 'rgba(220,53,69,1)',
-          data                : [28, 48, 40, 19, 86, 27, 250,123,23,56,33,12]
+          data                : [
+            @foreach ($year_expenditures as $year_expenditure)
+              {{ $year_expenditure . ',' }}
+            @endforeach
+        ]
         },
         {
           label               : 'Income',
@@ -200,7 +155,11 @@ Income Expenditure
           pointStrokeColor    : '#c1c7d1',
           pointHighlightFill  : '#fff',
           pointHighlightStroke: 'rgba(220,220,220,1)',
-          data                : [65, 59, 80, 81, 56, 55, 40,28, 48, 40, 19, 86]
+          data                : [
+            @foreach ($year_incomes as $year_income)
+              {{ $year_income . ',' }}
+            @endforeach
+          ]
         },
       ]
     }
@@ -209,7 +168,6 @@ Income Expenditure
     //- DONUT CHART -
     //-------------
     // Get context with jQuery - using jQuery's .get() method.
-    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
     var donutData        = {
       labels: [
           'Income',
@@ -218,7 +176,7 @@ Income Expenditure
       ],
       datasets: [
         {
-          data: [700.50,500.36],
+          data: [{{ $total_month_income }},{{ $total_month_expenditure }}],
           backgroundColor : ['#00a65a', '#dc3545'],
         }
       ]
@@ -227,13 +185,6 @@ Income Expenditure
       maintainAspectRatio : false,
       responsive : true,
     }
-    //Create pie or douhnut chart
-    // You can switch between pie and douhnut using the method below.
-    var donutChart = new Chart(donutChartCanvas, {
-      type: 'doughnut',
-      data: donutData,
-      options: donutOptions
-    })
 
     //-------------
     //- BAR CHART -
@@ -255,6 +206,45 @@ Income Expenditure
       type: 'bar',
       data: barChartData,
       options: barChartOptions
+    })
+     //- PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData        = donutData;
+    var pieOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    // You can switch between pie and douhnut using the method below.
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: pieData,
+      options: pieOptions
+    })
+    //---------------------
+    //- STACKED BAR CHART -
+    //---------------------
+    var stackedBarChartCanvas = $('#stackedBarChart').get(0).getContext('2d')
+    var stackedBarChartData = $.extend(true, {}, barChartData)
+
+    var stackedBarChartOptions = {
+      responsive              : true,
+      maintainAspectRatio     : false,
+      scales: {
+        xAxes: [{
+          stacked: true,
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    }
+
+    var stackedBarChart = new Chart(stackedBarChartCanvas, {
+      type: 'bar',
+      data: stackedBarChartData,
+      options: stackedBarChartOptions
     })
   });
 </script>
