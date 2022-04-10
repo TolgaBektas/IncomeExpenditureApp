2d07ca1505b4c89baac75eaa5d1b9ae670df99c8
Tolga Bektaş <tolgabektas00@gmail.com>
2022-04-10 21:30:20 +0300

    topmenu added highcharts.js added regular graph removed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,266 +1,228 @@
 @extends('layouts.admin')
 @section('title')
-Income Expenditure
+    Income Expenditure
 @endsection
 @section('css')
-
-@endsection
-@section('topmenu')
-<form action="" method="GET">
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Select Year
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" name="year" id="year">
-    @foreach ($years as $year)
-    <li><a class="dropdown-item" name="query" href="{{ route('index','q='.$year) }}">{{ $year }}</a></li>
-    @endforeach    
-  </ul>
-</div>
-</form>
 @endsection
 @section('content')
-
-<div class="row">
-  <div class="col-md-6">
-    {{-- donut chart --}}
     <div class="row">
-   <div class="col-md-6">
-    <!-- small box -->
-    <div class="small-box bg-success">
-      <div class="inner">
-        <h3>${{ $total_month_income }}</h3>
-        <p>Monthly Income</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-bag"></i>
-      </div>
-      <a href="{{ route('income.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col --> 
-  <div class="col-md-6">
-    <!-- small box -->
-    <div class="small-box bg-danger">
-      <div class="inner">
-        <h3>${{ $total_month_expenditure }}</h3>
-        <p>Monthly Expenditure</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-pie-graph"></i>
-      </div>
-      <a href="{{ route('expenditure.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->
-  <div class="col-md-6">
-    <!-- small box -->
-    <div class="small-box bg-success">
-      <div class="inner">
-        <h3>${{ $total_year_income }}</h3>
-        <p>Yearly Income</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-person-add"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Current Month Income-Expenditure Graphics</h3>
 
-  <div class="col-md-6">
-    <!-- small box -->
-    <div class="small-box bg-danger">
-      <div class="inner">
-        <h3>${{ $total_year_expenditure }}</h3>
-        <p>Yearly Expenditure</p>
-      </div>
-      <div class="icon">
-        <i class="ion ion-pie-graph"></i>
-      </div>
-      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div> 
-   <!-- ./col -->
-  </div>
-   {{-- donut chart --}}
-  </div>
-   
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>${{ $total_month_income }}</h3>
+                                <p>Monthly Income</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="{{ route('income.index') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>${{ $total_month_expenditure }}</h3>
+                                <p>Monthly Expenditure</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="{{ route('expenditure.index') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <figure class="highcharts-figure">
+                            <div id="container1"></div>
+                        </figure>
+                    </div>
 
-   {{-- pie chart --}}
-   <div class="col-md-6">     
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Current Month Income-Expenditure Graphics</h3>
 
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove">
-            <i class="fas fa-times"></i>
-          </button>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <canvas id="pieChart" style="min-height: 215px; height: 215px; max-height: 215px; max-width: 100%;"></canvas>
-      </div>
-      <!-- /.card-body -->
-    </div>
-   </div>
-   {{-- pie chart --}}
 
-   {{-- bar chart --}}
-   <div class="col-md-12">
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Current Year Income-Expenditure Graphics</h3>
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h3 class="card-title">Current Year Income-Expenditure Graphics</h3>
+                    </div>
 
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove">
-            <i class="fas fa-times"></i>
-          </button>
+                    <div class="col-md-4">
+                        <div class="card-tools float-end">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                        <form action="" method="GET" class="float-end">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Select Year
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" name="year" id="year">
+                                    @foreach ($years as $year)
+                                        <li><a class="dropdown-item" name="query"
+                                                href="{{ route('index', 'q=' . $year) }}">{{ $year }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <figure class="highcharts-figure">
+                            <div id="container2"></div>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>${{ $total_year_income }}</h3>
+                                <p>Yearly Income</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{ route('income.index') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>${{ $total_year_expenditure }}</h3>
+                                <p>Yearly Expenditure</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="{{ route('expenditure.index') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-        </div>
-      </div>
-      <!-- /.card-body -->
     </div>
-   </div>
-   {{-- bar chart --}}
-
-
 @endsection
 @section('js')
-<script>
-  $(function () {
-    /* ChartJS
-     * -------
-     * Here we will create a few charts using ChartJS
-     */
-     var areaChartData = {
-      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October','November', 'December'],
-      datasets: [
-        {
-          label               : 'Expenditure',
-          backgroundColor     : 'rgba(220,53,69,0.9)',
-          borderColor         : 'rgba(220,53,69,0.8)',
-          pointRadius          : false,
-          pointColor          : '#3b8bba',
-          pointStrokeColor    : 'rgba(220,53,69,1)',
-          pointHighlightFill  : '#fff',
-          pointHighlightStroke: 'rgba(220,53,69,1)',
-          data                : [
-            @foreach ($year_expenditures as $year_expenditure)
-              {{ $year_expenditure . ',' }}
-            @endforeach
-        ]
-        },
-        {
-          label               : 'Income',
-          backgroundColor     : 'rgba(60, 179, 113, 1)',
-          borderColor         : 'rgba(60, 179, 113, 1)',
-          pointRadius         : false,
-          pointColor          : 'rgba(60, 179, 113, 1)',
-          pointStrokeColor    : '#c1c7d1',
-          pointHighlightFill  : '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data                : [
-            @foreach ($year_incomes as $year_income)
-              {{ $year_income . ',' }}
-            @endforeach
-          ]
-        },
-      ]
-    }
-    
-    //-------------
-    //- DONUT CHART -
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    var donutData        = {
-      labels: [
-          'Income',
-          'Expenditure',
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+
+    <script>
+        Highcharts.chart('container1', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y:.1f}$</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.y:.1f}'
+
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Total Price',
+                colorByPoint: true,
+                data: [{
+                    color:'#28a745',
+                        name: 'Monthly Income',
+                        y: {{ $total_month_income }}
+                    },
+                    {
+                        color:'#dc3545',
+                        name: 'Monthly Expenditure',
+                        y: {{ $total_month_expenditure }}
+                    },
+                ]
+            }]
+        });
+        Highcharts.chart('container2', {
+            chart: {
+                type: 'column'              
+            },
+            
+            title: {
+                text: ''
+            },
+            xAxis: {
+                    categories:[
+
+                @foreach ($year_incomes as $key=>$value)
+                    '{{ $key }}',
+                @endforeach
+                ]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ''
+                }
+            },
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}$)<br/>',
+                shared: true
+            },
           
-      ],
-      datasets: [
-        {
-          data: [{{ $total_month_income }},{{ $total_month_expenditure }}],
-          backgroundColor : ['#00a65a', '#dc3545'],
-        }
-      ]
-    }
-    var donutOptions     = {
-      maintainAspectRatio : false,
-      responsive : true,
-    }
-
-    //-------------
-    //- BAR CHART -
-    //-------------
-    var barChartCanvas = $('#barChart').get(0).getContext('2d')
-    var barChartData = $.extend(true, {}, areaChartData)
-    var temp0 = areaChartData.datasets[0]
-    var temp1 = areaChartData.datasets[1]
-    barChartData.datasets[0] = temp1
-    barChartData.datasets[1] = temp0
-
-    var barChartOptions = {
-      responsive              : true,
-      maintainAspectRatio     : false,
-      datasetFill             : false
-    }
-
-    var barChart = new Chart(barChartCanvas, {
-      type: 'bar',
-      data: barChartData,
-      options: barChartOptions
-    })
-     //- PIE CHART -
-    //-------------
-    // Get context with jQuery - using jQuery's .get() method.
-    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-    var pieData        = donutData;
-    var pieOptions     = {
-      maintainAspectRatio : false,
-      responsive : true,
-    }
-    // You can switch between pie and douhnut using the method below.
-    var pieChart = new Chart(pieChartCanvas, {
-      type: 'pie',
-      data: pieData,
-      options: pieOptions
-    })
-    //---------------------
-    //- STACKED BAR CHART -
-    //---------------------
-    var stackedBarChartCanvas = $('#stackedBarChart').get(0).getContext('2d')
-    var stackedBarChartData = $.extend(true, {}, barChartData)
-
-    var stackedBarChartOptions = {
-      responsive              : true,
-      maintainAspectRatio     : false,
-      scales: {
-        xAxes: [{
-          stacked: true,
-        }],
-        yAxes: [{
-          stacked: true
-        }]
-      }
-    }
-
-    var stackedBarChart = new Chart(stackedBarChartCanvas, {
-      type: 'bar',
-      data: stackedBarChartData,
-      options: stackedBarChartOptions
-    })
-  });
-</script>
+            series: [{
+                color:'#28a745',
+                name: 'Income',
+                data: [  
+                    @foreach ($year_incomes as $key=>$value)
+                    {{ $value }},
+                @endforeach
+            ]
+            }, {
+                color:'#dc3545',
+                name: 'Expenditure',
+                data: [
+                    @foreach ($year_expenditures as $key=>$value)
+                    {{ $value }},
+                @endforeach
+                ]
+            }]
+        });
+    </script>
 @endsection
