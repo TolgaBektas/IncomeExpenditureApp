6f07356dce2813c31d4e85f706d9ddef1a2c77bd
Tolga Bektaş <tolgabektas00@gmail.com>
2022-05-07 18:40:29 +0300

    seed data increased

--- database/seeders/ExpenditureSeeder.php
+++ database/seeders/ExpenditureSeeder.php
@@ -17,13 +17,13 @@ class ExpenditureSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        for ($i = 0; $i < 150; $i++) {
+        for ($i = 0; $i < 300; $i++) {
             DB::table('expenditure')->insert([
                 'description' => $faker->text(100),
                 'category_id' =>  $faker->numberBetween(1, 5),
                 'price' => $faker->randomFloat(2, 10, 10000),
                 'invoice' => "invoices/expenditure/test.pdf",
-                'invoice_date' => $faker->dateTimeBetween('-3 years', '+2 week'),
+                'invoice_date' => $faker->dateTimeBetween('-3 years', '+1 year'),
                 'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
                 'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
             ]);
